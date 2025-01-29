f77730e166e02f4f202e2a2cbeae56741fafcd09
Harutyun Matosoghlyan <143116182+harutyunmatosoghlya@users.noreply.github.com>
2025-01-29 22:43:54 +0400

    main

---
+++ src/main/java/am/itspace/shoesmarket/ShoesMarketApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.shoesmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoesMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/itspace/shoesmarket/ShoesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package am.itspace.shoesmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
