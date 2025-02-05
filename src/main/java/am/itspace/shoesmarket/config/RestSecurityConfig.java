ff5915b780eceae8d58669bba038243cdf9a8137
Harutyun Matosoghlyan <143116182+harutyunmatosoghlya@users.noreply.github.com>
2025-02-05 16:14:50 +0400

    created separate classes for AuthenticationProvider and the PasswordEncoder. deleted a comment in the JwtTokenUtil class

--- src/main/java/am/itspace/shoesmarket/config/RestSecurityConfig.java
+++ src/main/java/am/itspace/shoesmarket/config/RestSecurityConfig.java
@@ -6,21 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class RestSecurityConfig {
-
-    private final PasswordEncoder passwordEncoder;
-    private final UserDetailsService userDetailsService;
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
     private final JWTAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
@@ -32,23 +25,15 @@ public class RestSecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET,"/swagger-ui/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
                 .requestMatchers("/v3/api-docs/**").permitAll()
-                .requestMatchers(HttpMethod.POST,"/user/login").permitAll()
-                .requestMatchers(HttpMethod.POST,"/user/register").permitAll()
-                .requestMatchers(HttpMethod.GET,"/authors/**").authenticated()
+                .requestMatchers(HttpMethod.POST, "/user/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/user/register").permitAll()
+                .requestMatchers(HttpMethod.GET, "/authors/**").authenticated()
                 .anyRequest().authenticated();
 
         httpSecurity.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder);
-        return authenticationProvider;
     }
 }
--- src/main/java/am/itspace/shoesmarket/util/JwtTokenUtil.java
+++ src/main/java/am/itspace/shoesmarket/util/JwtTokenUtil.java
@@ -54,7 +54,6 @@ public class JwtTokenUtil {
 
     public String generateToken(String email) {
         Map<String, Object> claims = new HashMap<>();
-//        claims.put("phone", "098765432");
         return doGenerateToken(claims, email);
     }
 
