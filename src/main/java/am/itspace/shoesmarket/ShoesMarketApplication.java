91cd66afb17c80f7ba6953ae9966d7ffd0e770ea
Harutyun Matosoghlyan <143116182+harutyunmatosoghlya@users.noreply.github.com>
2025-02-05 16:25:15 +0400

    delete PasswordEncoderBean in ShoesMarketApplication

--- src/main/java/am/itspace/shoesmarket/ShoesMarketApplication.java
+++ src/main/java/am/itspace/shoesmarket/ShoesMarketApplication.java
@@ -2,18 +2,10 @@ package am.itspace.shoesmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ShoesMarketApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoesMarketApplication.class, args);
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
